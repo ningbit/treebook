2832d4c535a03da358f1a3c63fd238f183af6ced
Ning Yap <ning.yap@gmail.com>
2013-03-28 17:42:11 -0400

    Scoped status updates to the current user on create and update.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,24 +10,24 @@ class User < ActiveRecord::Base
   				  :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
-validates :first_name, presence: true
+  validates :first_name, presence: true
 
-validates :first_name, presence: true
+  validates :first_name, presence: true
 
-validates :last_name, presence: true
+  validates :last_name, presence: true
 
-validates :profile_name, presence: true, 
-                         uniqueness: true,
-                         format: {
-                          with: /^[a-zA-Z0-9_-]+$/,
-                          message: "must be formatted correctly."
-                         }
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                           format: {
+                            with: /^[a-zA-Z0-9_-]+$/,
+                            message: "must be formatted correctly."
+                           }
 
 
-has_many :statuses
+  has_many :statuses
 
   def full_name
-  	first_name + " " + last_name
+      first_name + " " + last_name
   end
 
   def gravatar_url
--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -31,9 +31,23 @@ class StatusesControllerTest < ActionController::TestCase
 
   test "should create status when logged in" do
     sign_in users(:ning)
+
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content }
+      post :create, status: { content: @status.content, user_id: @status.user.id }
     end
+
+    assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:ning)
+   
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+    end
+
+  assert_redirected_to status_path(assigns(:status))
+  assert_equal assigns(:status).user_id, users(:ning).id
   end
 
   test "should show status" do
@@ -58,11 +72,28 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should update status for the current user when logged in" do
+    sign_in users(:ning)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ning).id
+  end
+
+  test "should not update the status if nothing has changed" do
+    sign_in users(:ning)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ning).id
+  end
+
+
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
       end
     end
+
+
 
 end
 
